b8ebff4d4d3cc513935f8cc631ce43a675de6c11
Vincen <vincenhayate123@gmail.com>
2024-11-20 20:22:50 +0700

    Minor fix

---
+++ src/pages/staff/view.tsx
@@ -0,0 +1,11 @@
+import { Image } from "lucide-react";
+
+export default function ViewPage() {
+  return (
+    <>
+      <div className="w-52 h-64 flex justify-center items-center bg-black">
+        <Image />
+      </div>
+    </>
+  );
+}
