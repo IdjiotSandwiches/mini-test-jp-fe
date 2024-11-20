95b73d68709719b803c8b084e6c2ba182c3bac7e
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-11-20 16:15:46 +0700

    Schedule (#7)
    * Add table component
    
    * Create datetime picker component
    
    * Create Staff landing page
    
    * Rename & move to staff folder
    
    * Add style
    
    * Add table
    
    * create needed pages
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

---
+++ src/pages/staff/index.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { DateTimePicker } from "@/components/ui/datetime-picker";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function SchedulePage() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [startTime, setStartTime] = React.useState<Date | undefined>(
+    new Date(0, 0, 0, 0, 0, 0, 0)
+  );
+  const [endTime, setEndTime] = React.useState<Date | undefined>(
+    new Date(0, 0, 0, 0, 0, 0, 0)
+  );
+
+  const data = { date, startTime, endTime };
+  const state = { setDate, setStartTime, setEndTime };
+
+  const formatDate = (date?: Date) => {
+    const formattedDate = new Intl.DateTimeFormat("en-ID", {
+      dateStyle: "full",
+    }).format(date);
+
+    return formattedDate;
+  };
+
+  const formatTime = (time?: Date) => {
+    const formattedTime = new Intl.DateTimeFormat("en-ID", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }).format(time);
+
+    return formattedTime;
+  };
+
+  return (
+    <>
+      <div className="min-h-screen max-w-screen-lg mx-auto flex items-center">
+        <div className="flex gap-10 w-full">
+          <DateTimePicker data={data} state={state} />
+          <div className="flex flex-col gap-2 w-full">
+            <div>
+              <h2 className="text-3xl font-bold">{formatDate(date)}</h2>
+              <h5 className="text-lg font-medium">
+                {formatTime(startTime)} - {formatTime(endTime)}
+              </h5>
+            </div>
+            <div>
+              <Button className="bg-sky-500">INSERT</Button>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="">Nama</TableHead>
+                    <TableHead>NIM</TableHead>
+                    <TableHead>Tanggal Sidang</TableHead>
+                    <TableHead className="">Action</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="font-medium">Idjiot Sandwiches</TableCell>
+                    <TableCell>2118033615</TableCell>
+                    <TableCell>{formatDate(date)}</TableCell>
+                    <TableCell className="flex items-center gap-2">
+                      <Button className="bg-lime-500">VIEW</Button>
+                      <Button className="bg-orange-400">UPDATE</Button>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
